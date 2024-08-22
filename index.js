37feaf768a58f42b69955b6f70e12fd05ed08079
ritwick-quadranttech <ritwick@quadranttech.co>
2024-08-22 18:33:11 +0530

    responsive,mobile nav allow

---
+++ index.js
@@ -0,0 +1,73 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable:true
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+  });
+// ------------loder tranform------------
+const sections = document.querySelectorAll("section");
+const observer_opacity = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("visible");
+        }
+    });
+});
+sections.forEach((client) => {
+    observer_opacity.observe(client);
+});
+
+
+
+
+// mobile nav
+let navbtn = document.querySelector('.nav-btn')
+
+let body = document.querySelector('body')
+let slidebar = document.querySelector('.slider-bar');
+navbtn.addEventListener('click', () => {
+    slidebar.classList.toggle('active')
+    navbtn.classList.toggle('active')
+})
+var lastscroltop = 0;
+window.addEventListener("scroll", function () {
+    var scrolToTop = window.scrollY || this.document.documentElement.scrollTop
+    if (scrolToTop > lastscroltop) {
+        slidebar.classList.remove("active")
+
+    }
+})
+
+// catagorylists options
+let catagorylists = document.querySelectorAll(".catagory-list");
+let textOptions = document.querySelectorAll(".list");
+if (catagorylists) {
+    catagorylists[0].classList.add("active")
+    textOptions[0].classList.add("active")
+    catagorylists.forEach((item, index) => {
+        console.warn(index);
+        
+        item.addEventListener("click", () => {
+            textOptions.forEach(option => {
+                option.classList.remove("active")
+            })
+            catagorylists.forEach(menuItem => {
+                menuItem.classList.remove("active");
+            })
+            textOptions[index].classList.add("active")
+            item.classList.add("active");
+        })
+    })
+}
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -34,7 +34,7 @@ module.exports = {
         screens: {
           DEFAULT: '100%',
           sm: '600px',
-          md: '728px',
+          md: '880px',
           lg: '984px',
           xl: '1240px',
           '2xl': '1396px'
